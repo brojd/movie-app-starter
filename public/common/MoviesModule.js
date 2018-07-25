c1be4b6e6717d04017777850945a9f66317a475e
Aleksandra Zubrzak <aleksandra.zubrzak@gmail.com>
2018-07-25 20:56:17 +0200

    create moviesStub

---
+++ public/common/MoviesModule.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+var MoviesModule = (function() {
+    var moviesStub = [
+        {
+            title: "aaa",
+            imgSrc: "https://1.fwcdn.pl/po/42/56/464256/7317256.3.jpg",
+            genre: "bbb",
+            year: "ccc",
+            mark: 8
+        },
+        {
+            title: "aaa",
+            imgSrc: "https://1.fwcdn.pl/po/42/56/464256/7317256.3.jpg",
+            genre: "bbb",
+            year: "ccc",
+            mark: 8
+        },
+        {
+            title: "aaa",
+            imgSrc: "https://1.fwcdn.pl/po/42/56/464256/7317256.3.jpg",
+            genre: "bbb",
+            year: "ccc",
+            mark: 8
+        },
+        {
+            title: "aaa",
+            imgSrc: "https://1.fwcdn.pl/po/42/56/464256/7317256.3.jpg",
+            genre: "bbb",
+            year: "ccc",
+            mark: 8
+        },
+    ];
+    
+    return {
+        moviesStub: moviesStub
+    }
+
+})()
---
+++ public/list-of-movies/js/list.js
@@ -0,0 +1,2 @@
+'use strict';
+console.log(MoviesModule.moviesStub);
